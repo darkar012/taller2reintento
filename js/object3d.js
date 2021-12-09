f50028ab3c707d1980e14d54cc9a6f1eb47962ec
xlHenshin <andreslayer117@gmail.com>
2021-12-09 01:01:42 -0500

    3D working but not fine at all

--- js/object3d.js
+++ js/object3d.js
@@ -1,53 +1,63 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.135.0';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../js/GLTFLoader.js';
 
 let scene, camera, renderer;
 
-function init(){
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,100);
+renderer = new THREE.WebGLRenderer({antialias:true});
 
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xdddddd);
+scene.background = new THREE.Color(0xFFFFFF);
 
-    camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight,1,5000);
-    camera.rotation.y = 45/180*Math.PI;
-    camera.position.x = 800;
-    camera.position.y = 100;
-    camera.position.z = 1000;
+camera.position.set( 5, 2, 8 );
+/*camera.rotation.y = 45/180*Math.PI;
+camera.position.x = 800;
+camera.position.y = 100;
+camera.position.z = 1000;*/
 
-    let hlight = new THREE.AmbientLight (0x404040, 100);
-    scene.add(hlight);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener('change', renderer);
 
-    let directionalLight = new THREE.DirectionalLight(0xffffff,100);
-    directionalLight.castShadow = true;
-    scene.add(directionalLight);
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-    let light1 = new THREE.PointLight(0xc4c4c4,10);
-    light1.position.set(0,300,500);
-    scene.add(light1);
+let hlight = new THREE.AmbientLight (0x404040, 100);
+scene.add(hlight);
 
-    let light2 = new THREE.PointLight(0xc4c4c4,10);
-    light2.position.set(500,100,0);
-    scene.add(light2)
+let directionalLight = new THREE.DirectionalLight(0xffffff,100);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
-    let light3 = new THREE.PointLight(0xc4c4c4,10);
-    light3.position.set(0,100,-500);
-    scene.add(light3)
+let light1 = new THREE.PointLight(0xc4c4c4,10);
+light1.position.set(0,300,500);
+scene.add(light1);
 
-    let light4 = new THREE.PointLight(0xc4c4c4,10);
-    light4.position.set(-500,300,0);
-    scene.add(light4)
+let light2 = new THREE.PointLight(0xc4c4c4,10);
+light2.position.set(500,100,0);
+scene.add(light2)
 
-    renderer = new THREE.WebGLRenderer({antialias:true});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+let light3 = new THREE.PointLight(0xc4c4c4,10);
+light3.position.set(0,100,-500);
+scene.add(light3)
 
-    let loader = new THREE.GLTFLoader();
+let light4 = new THREE.PointLight(0xc4c4c4,10);
+light4.position.set(-500,300,0);
+scene.add(light4)
 
-    loader.load("./model/scene.gltf", function(gltf){
-        car = gltf.scene.children[0];
-        car.scale.set(0.5,0.5,0.5);
-        scene.add(gltf.scene);
-        renderer.render(scene,camera);
-    })
+const loader = new GLTFLoader();
+loader.load("./models/scene.gltf", function(gltf){
+    //let car = gltf.scene.children[0];
+    //car.scale.set(0.5,0.5,0.5);
+    scene.add(gltf.scene);
+}, undefined, function ( error ) {
+
+    console.error( error );
+} );
+
+function animate() {
+    renderer.render(scene,camera); 
+    requestAnimationFrame(animate);  
 }
 
-init();
+animate();
