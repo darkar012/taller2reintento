0167d1ff7a0433cf6396c05e76b47de1953a85aa
xlHenshin <andreslayer117@gmail.com>
2021-12-09 04:13:57 -0500

    3d Models Working

--- js/object3d.js
+++ js/object3d.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
-import { getAuth, onAuthStateChanged, } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
 
 import * as THREE from 'https://cdn.skypack.dev/three@0.135.0';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/controls/OrbitControls.js';
@@ -9,56 +8,82 @@ import { GLTFLoader } from '../js/GLTFLoader.js';
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth();
 
-const url = window.location.search;
-const searchParas = new URLSearchParams(url);
-let productId = searchParas.get("id").replace('"', "");
+let productId;
+let productName;
 
-let scene, camera, renderer;
+const getProduct = async () => {
+    const url = window.location.search;
+    const searchParas = new URLSearchParams(url);
+    productId = searchParas.get("id").replace('"', "");
 
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-renderer = new THREE.WebGLRenderer({ antialias: true });
+    const docRef = doc(db, "products", productId);
+    const docSnap = await getDoc(docRef);
+    const data = docSnap.data();
 
-scene.background = new THREE.Color(0xE5E5E5);
+    productName = data.name;
+    console.log(productName)
 
-camera.position.set(2, 2, 0);
+    renderProduct();
+};
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.addEventListener('change', renderer);
+const renderProduct = () =>{
+    let scene, camera, renderer;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    
+    scene.background = new THREE.Color(0xE5E5E5);
+    
+    camera.position.set(2, 2, 0);
+    
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', renderer);
+    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    let hlight = new THREE.AmbientLight(0x404040, 100);
+    scene.add(hlight);
+    
+    let directionalLight = new THREE.DirectionalLight(0xffffff, 10);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+    
+    const loader = new GLTFLoader();
+    
+    if (productName === "Xperia 1 III") {
 
-let hlight = new THREE.AmbientLight(0x404040, 100);
-scene.add(hlight);
+        loader.load("./models/xperia/scene.gltf", function (gltf) {
+            scene.add(gltf.scene);
+        }, undefined, function (error) {
+            console.error(error);
+        });
 
-let directionalLight = new THREE.DirectionalLight(0xffffff, 10);
-directionalLight.castShadow = true;
-scene.add(directionalLight);
+    }else if (productName === "DualSense PS5") {
 
-const loader = new GLTFLoader();
-loader.load("./models/xperia/scene.gltf", function (gltf) {
-    scene.add(gltf.scene);
-}, undefined, function (error) {
+        loader.load("./models/ps5dualsense/scene.gltf", function (gltf) {
+            scene.add(gltf.scene);
+        }, undefined, function (error) {
+            console.error(error);
+        });
 
-    console.error(error);
-});
+    }else if(productName === "MDR-ZX310AP"){
 
-function animate() {
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
+        loader.load("./models/sony/scene.gltf", function (gltf) {
+            scene.add(gltf.scene);
+        }, undefined, function (error) {
+            console.error(error);
+        });
+    }
+
+    function animate() {
+        renderer.render(scene, camera);
+        requestAnimationFrame(animate);
+    }
+    
+    animate();
 }
 
-animate();
-
-onAuthStateChanged(auth, async (user) => {
-    if (user) {
-        
-        userLogged = user;
-    } else {
-        
-    }
-    //getProduct();
-});
+getProduct();
